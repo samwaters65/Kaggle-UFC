cd5566b6041003ee98b3e9e15827225ac8898fc1
samwaters65 <samwaters65@gmail.com>
2019-12-20 21:34:00 -0500

    Final Update
    Formatting changes for this simple exercise.

--- Project/run.py
+++ Project/run.py
@@ -15,7 +15,9 @@ data = pd.read_csv(f"{c.filesDir}data.csv")
 # New Features
 data["USAFight"] = data.location.apply(f.isUSA)
 data["WinnerTarget"] = data.Winner.apply(f.winner)
-data = pd.get_dummies(data, columns=["weight_class", "B_Stance", "R_Stance"])
+data = pd.get_dummies(data, columns=["weight_class",
+                                         "B_Stance",
+                                         "R_Stance"])
 
 data.drop(axis=1, columns=c.unusedCols, inplace=True)
 beforeLen = data.shape[0]
@@ -28,14 +30,13 @@ print(f"{round((postLen - beforeLen)/beforeLen, 4)*-100}% of records removed")
 
 model, baseline_test_score = f.runLogReg(data_noNA) 
 
-##############
-# NEED CORRELATION REVIEW TO PRE-REMOVE VARIABLES. Then go through NA removal again (hopefully MORE records stay) 
-
  
 # Scaled Model:
 
 scaled_data = f.scaleXData(data_noNA) 
-scaled_data.insert(loc=scaled_data.shape[1], column='WinnerTarget', value=data_noNA.WinnerTarget)
+scaled_data.insert(loc=scaled_data.shape[1],
+                   column='WinnerTarget',
+                   value=data_noNA.WinnerTarget)
 
 scaledModel, scaled_test_score = f.runLogReg(scaled_data)
 
@@ -46,7 +47,9 @@ first_iter_x_cols = f.findVars(scaled_data, importance_threshold = 0.005)
 print(f'Number of X_vars: {len(first_iter_x_cols)}')
 
 data_reduced1 = data[first_iter_x_cols]
-data_reduced1.insert(loc=data_reduced1.shape[1], column='WinnerTarget', value=data.WinnerTarget)
+data_reduced1.insert(loc=data_reduced1.shape[1],
+                     column='WinnerTarget',
+                     value=data.WinnerTarget)
 
 beforeLen = data_reduced1.shape[0]
 data_reduced1_noNA = data_reduced1.dropna(axis=0, how="any")
@@ -54,7 +57,9 @@ postLen = data_reduced1_noNA.shape[0]
 print(f"{round((postLen - beforeLen)/beforeLen, 4)*-100}% of records removed")
 
 data_reduced1_scaled = f.scaleXData(data_reduced1_noNA)
-data_reduced1_scaled.insert(loc=data_reduced1_scaled.shape[1], column='WinnerTarget', value=data_reduced1_noNA.WinnerTarget)
+data_reduced1_scaled.insert(loc=data_reduced1_scaled.shape[1],
+                            column='WinnerTarget',
+                            value=data_reduced1_noNA.WinnerTarget)
 
 firstIterModel, firstIter_test_score = f.runLogReg(data_reduced1_scaled)
 
@@ -64,7 +69,9 @@ second_iter_x_cols = f.findVars(data_reduced1_noNA, importance_threshold=0.015)
 print(f'Number of X_vars: {len(second_iter_x_cols)}')
 
 data_reduced = data[second_iter_x_cols]
-data_reduced.insert(loc=data_reduced.shape[1], column='WinnerTarget', value=data.WinnerTarget)
+data_reduced.insert(loc=data_reduced.shape[1],
+                    column='WinnerTarget',
+                    value=data.WinnerTarget)
 
 
 beforeLen = data_reduced.shape[0]
@@ -75,7 +82,9 @@ print(f"{round((postLen - beforeLen)/beforeLen, 4)*-100}% of records removed")
 
 
 data_reduced2_scaled = f.scaleXData(data_reduced2_noNA)
-data_reduced2_scaled.insert(loc=data_reduced2_scaled.shape[1], column='WinnerTarget', value=data_reduced2_noNA.WinnerTarget)
+data_reduced2_scaled.insert(loc=data_reduced2_scaled.shape[1],
+                            column='WinnerTarget',
+                            value=data_reduced2_noNA.WinnerTarget)
 secondIterModel, secondIter_test_score = f.runLogReg(data_reduced2_scaled)
 
 
@@ -85,7 +94,9 @@ third_iter_x_cols = f.findVars(data_reduced2_noNA, importance_threshold=0.15)
 print(f'Number of X_vars: {len(third_iter_x_cols)}')
 
 data_reduced = data[third_iter_x_cols]
-data_reduced.insert(loc=data_reduced.shape[1], column='WinnerTarget', value=data.WinnerTarget)
+data_reduced.insert(loc=data_reduced.shape[1],
+                    column='WinnerTarget',
+                    value=data.WinnerTarget)
 
 
 beforeLen = data_reduced.shape[0]
@@ -96,38 +107,7 @@ print(f"{round((postLen - beforeLen)/beforeLen, 4)*-100}% of records removed")
 
 
 data_reduced3_scaled = f.scaleXData(data_reduced3_noNA)
-data_reduced3_scaled.insert(loc=data_reduced3_scaled.shape[1], column='WinnerTarget', value=data_reduced3_noNA.WinnerTarget)
+data_reduced3_scaled.insert(loc=data_reduced3_scaled.shape[1],
+                            column='WinnerTarget',
+                            value=data_reduced3_noNA.WinnerTarget)
 thirdIterModel, thirdIter_test_score = f.runLogReg(data_reduced3_scaled)
-
-
-#####################
-# Remove all of the unnecessary variables and then run again through findVars(). Then, remove the vars
-# from FULL data and run through process again.
-
-
-
-
-X = scaled_data.drop(axis=1, columns=["WinnerTarget"])
-y = scaled_data.WinnerTarget
-
-X_train, X_test, y_train, y_test = train_test_split(
-    X, y, test_size=0.3, random_state=77
-)
-
-clf = LogisticRegression(
-    penalty="elasticnet",
-    random_state=64,
-    solver="saga",
-    max_iter=100
-    # multi_class="auto",
-).fit(X_train, y_train)
-
-y_preds = clf.predict(X_test)
-
-print(f"Base Score: {round(clf.score(X_test, y_test),4)*100}%")
-
-## Feature selection, balancing?, scaling
-
-
-### Need to use random forest classifier for
-### Idea: Test if Referee is statistically biased!
